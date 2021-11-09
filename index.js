22dd25590e541d9e23ddbc4781be4cfb9e2b4a39
pawneetdev <pawneet.singh@NTPL-ITMACLAP0143.local>
2021-11-09 15:00:41 +0530

    params and query

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/courses', (req, res) => {
+	res.send([1,2,3]);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+	res.send({
+		params: req.params,
+		query: req.query
+	});
 });
 
 app.listen(port, () => {
